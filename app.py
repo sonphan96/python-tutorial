aeb30fd3d7e175ba784a4912ea5c896181e6374a
Son Phan <mac@Son-Phans-MacBook-Pro.local>
2020-05-12 19:50:27 -0400

    first commit

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-x = 20000
-print(x)
+items = [
+    ("Product1", 10),
+    ("Product2", 9),
+    ("Product3", 15)
+]
+
+
+def sort_item(item):
+    return item[1]
+
+
+items.sort(key=sort_item)
+print(items)
