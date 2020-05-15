2d8846db1d595553d7101901bf72de033b35fb5c
Son Phan <mac@Son-Phans-MacBook-Pro.local>
2020-05-14 23:24:51 -0400

    second

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-items = [
-    ("Product1", 10),
-    ("Product2", 9),
-    ("Product3", 15)
-]
+# items = [
+#     ("Product1", 10),
+#     ("Product2", 9),
+#     ("Product3", 15)
+# ]
 
 
-# def sort_item(item):
-#     return item[1]
+# # def sort_item(item):
+# #     return item[1]
 
 
-items.sort(key=lambda item: item[1])
-print(items)
+# items.sort(key=lambda item: item[1])
+# print(items)
+
+
+try:
+    age = int(input("Age: "))
+    xfactor = 10 / age
+except (ValueError, ZeroDivisionError):
+    print("You didn't enter a valid age")
+else:
+    print("No exception were thrown")
+print("execution continue")
