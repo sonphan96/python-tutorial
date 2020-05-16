a21e10844a0d27c2e7141a78732dd2115269183a
Son Phan <mac@Son-Phans-MacBook-Pro.local>
2020-05-16 00:06:44 -0400

    third 12:06 16 may

--- app.py
+++ app.py
@@ -1,31 +1,6 @@
-class Point:
-    default_color = "red"
+class Mammal:
+    def eat(self):
+        print("eat")
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    @classmethod
-    def zero(cls):
-        return cls(0, 0)
-
-    def __str__(self):
-        return f"Point ({self.x}, {self.y})"
-
-    def draw(self):
-        print(f"Point ({self.x}, {self.y})")
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __gt__(self, other):
-        return self.x > other.x and self.y > other.y
-
-    def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y)
-
-
-point = Point(1, 2)
-other = Point(1, 2)
-
-print(point + other)
+    def walk(self):
+        print("walk")
