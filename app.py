882064588f5368750d1c35c4e6d926a03bfef36f
Son Phan <mac@Son-Phans-MacBook-Pro.local>
2020-05-17 18:29:31 -0400

    6:29 PM - May 17 2020

--- app.py
+++ app.py
@@ -1,6 +1,1 @@
-class Mammal:
-    def eat(self):
-        print("eat")
-
-    def walk(self):
-        print("walk")
+from ecommerce import sales
---
+++ ecommerce/sales.py
@@ -0,0 +1,12 @@
+
+def calc_tax():
+    pass
+
+
+def calc_shipping():
+    pass
+
+
+if __name__ == "__main__":
+    print("Sales started")
+    calc_tax()
